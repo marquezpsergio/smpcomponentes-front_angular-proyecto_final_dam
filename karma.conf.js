a3dc69b921b78348216d24a87d38905a0c056632
Sergio Márquez <s.marquez@preving.com>
2021-06-04 08:23:05 +0200

    UPDATED - FIXED - Cambio de los nombres de 'componentesSMP' a 'SMPComponentes'

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/componentesSMP'),
+      dir: require('path').join(__dirname, './coverage/SMPComponentes'),
       subdir: '.',
       reporters: [
         {type: 'html'},
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'componentesSMP'`, () => {
+  it(`should have as title 'SMPComponentes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('componentesSMP');
+    expect(app.title).toEqual('SMPComponentes');
   });
 
   it('should render title', () => {
